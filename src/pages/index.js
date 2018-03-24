6c0daa0aa022d3fe2ea4a32d4fbdc912fbc9a6ba
eujain <eujain@gmail.com>
2018-03-24 13:33:19 -0400

    added links to all projects to index.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,37 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import PostLink from "../components/post-link";
 
-const IndexPage = () => (
+const IndexPage = ({ data: { allMarkdownRemark: { edges } } }) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />);
+
+  return (
   <div>
     <h1>Selected Projects</h1>
     <p>TODO: generate list of projects here.</p>
 
-    <Link to="/page-2/">Go to page 2</Link>
+    <div>{Posts}</div>
   </div>
-)
+  )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`;
